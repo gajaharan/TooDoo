a8224abb230a50f1ad85a909cfab8a76cf0c0b7d
Gajaharan Satkunanandan <gajaharan@gmail.com>
2016-10-22 19:46:43 +0100

    Initial Commit

---
+++ TooDoo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TooDoo
+//
+//  Created by Gajaharan Satkunanandan on 22/10/2016.
+//  Copyright © 2016 Gajaharan Satkunanandan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TooDoo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TooDoo
+//
+//  Created by Gajaharan Satkunanandan on 22/10/2016.
+//  Copyright © 2016 Gajaharan Satkunanandan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
