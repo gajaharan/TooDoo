93c8191fee69a1b60b4d296041da97b4a39f7bcf
gajaharan <gajaharan@gmail.com>
2016-10-23 12:06:19 +0100

    *Colour coded the table cells *Setup TableViewDataSource

--- TooDoo/ViewController.m
+++ TooDoo/ViewController.m
@@ -26,6 +26,16 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    self.tableView.dataSource = self;
+    self.tableView.delegate = self;
+    
+    
+    NSArray *taskAsPropertyLists = [[NSUserDefaults standardUserDefaults] arrayForKey:TASK_OBJECTS_KEY];
+    
+    for(NSDictionary *dictionary in taskAsPropertyLists) {
+        Task *taskObject = [self taskObjectForDictionary:dictionary];
+        [self.taskObjects addObject:taskObject];
+    }
 }
 
 -(void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
@@ -73,5 +83,77 @@
 -(NSDictionary *)taskObjectAsAPropertyList:(Task *)taskObject {
     NSDictionary *dictionary = @{TASK_TITLE: taskObject.title, TASK_DESCRIPTION: taskObject.decription, TASK_DATE: taskObject.date, TASK_COMPLETION: @(taskObject.isCompleted) };
     return dictionary;
+}
+
+-(Task *)taskObjectForDictionary:(NSDictionary *) dictionary {
+    Task *taskObject = [[Task alloc] initWithData:dictionary];
+    
+    return taskObject;
+}
+
+-(BOOL)isDateGreaterThanDate:(NSDate *) date and:(NSDate *) toDate {
+    NSTimeInterval dateInterval = [date timeIntervalSince1970];
+    NSTimeInterval toDateInterval = [toDate timeIntervalSince1970];
+    
+    if(dateInterval > toDateInterval)
+        return YES;
+    else
+        return NO;
+}
+
+-(void)updateCompletionOfTask:(Task *) task forIndexPath:(NSIndexPath *) indexPath {
+    NSMutableArray *taskObjectsAsPropertyLists = [[[NSUserDefaults standardUserDefaults]arrayForKey:TASK_OBJECTS_KEY] mutableCopy];
+    
+    if(!taskObjectsAsPropertyLists) taskObjectsAsPropertyLists = [[NSMutableArray alloc]init];
+    
+    [taskObjectsAsPropertyLists removeObjectAtIndex:indexPath.row];
+    if(task.isCompleted == YES) task.isCompleted = NO;
+    else task.isCompleted = YES;
+    
+    [taskObjectsAsPropertyLists insertObject:[self taskObjectAsAPropertyList:task] atIndex:indexPath.row];
+    
+    [[NSUserDefaults standardUserDefaults] setObject:taskObjectsAsPropertyLists forKey:TASK_OBJECTS_KEY];
+    [[NSUserDefaults standardUserDefaults] synchronize];
+    
+    [self.tableView reloadData];
+    
+}
+
+#pragma UITableViewDataSource
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return [self.taskObjects count];
+}
+
+-(NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 1;
+}
+
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    static NSString *cellIdentifier = @"Cell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellIdentifier forIndexPath:indexPath];
+    
+    //Configure Cell
+    Task *task = self.taskObjects [indexPath.row];
+    cell.textLabel.text = task.title;
+    NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
+    [formatter setDateFormat:@"yyyy-MM-dd"];
+    NSString *stringFromDate = [formatter stringFromDate:task.date];
+    cell.detailTextLabel.text = stringFromDate;
+    
+    
+    BOOL isOverDue = [self isDateGreaterThanDate:[NSDate date] and:task.date];
+    if(task.isCompleted == YES) cell.backgroundColor = [UIColor greenColor];
+    else if(isOverDue == YES) cell.backgroundColor = [UIColor redColor];
+    else cell.backgroundColor = [UIColor yellowColor];
+    
+    return cell;
+}
+
+#pragma mark - UITableViewDelegate
+
+-(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    Task * task = self.taskObjects[indexPath.row];
+    [self updateCompletionOfTask:task forIndexPath:indexPath];
+    
 }
 @end
